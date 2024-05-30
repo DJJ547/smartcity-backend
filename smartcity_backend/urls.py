62e4a19dcfa74654f34cc5b78628548619a862d1
yifufang <yffang01@gmail.com>
2024-05-30 15:24:36 -0700

    drone

--- smartcity_backend/urls.py
+++ smartcity_backend/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('dashboard/', include('data_backend.urls')),
     path('iot/', include('iot_backend.urls')),
     path('camera/', include('camera_backend.urls')),
+    path('drone/', include('drone_backend.urls')),
 ]
